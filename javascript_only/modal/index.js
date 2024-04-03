8fded5c77ba79f16f6b8a744d1398e21eb160060
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-02 20:03:24 -0500

    queried dom elements and set them to variables

---
+++ javascript_only/modal/index.js
@@ -0,0 +1,10 @@
+// get modal element
+const modal = document.getElementById('myModal');
+
+// get button that opens the modal
+const btn = document.getElementById('openModalBtn');
+
+// get the element that closes the modal
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+
