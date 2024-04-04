a06d2bf39baec632c5172712a6468c9abd259083
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-03 20:41:55 -0500

    finished basic sidebar

--- react_components/version_1/src/components/sidebar/SideBar.jsx
+++ react_components/version_1/src/components/sidebar/SideBar.jsx
@@ -1,5 +1,31 @@
-import "./SideBar.css" 
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
+// import "./SideBar.css";
+
+// const SideBar = () => {
+//     const [isOpen, setIsOpen] = useState(false);
+
+//     const toggleSideBar = () => setIsOpen(!isOpen);
+
+//     return (
+//         <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+//             <button className="toggle-button" onClick={toggleSideBar}>☰</button>
+//             <ul className="sidebar-links">
+//                 <li><Link to="/navbar" onClick={toggleSideBar}>NavBar</Link></li>
+//                 <li><Link to="/sidebar" onClick={toggleSideBar}>SideBar</Link></li>
+//                 <li><Link to="/search" onClick={toggleSideBar}>Search</Link></li>
+//                 <li><Link to="/form" onClick={toggleSideBar}>Form</Link></li>
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export default SideBar;
+
+
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./SideBar.css";
 
 const SideBar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -7,8 +33,18 @@ const SideBar = () => {
     const toggleSideBar = () => setIsOpen(!isOpen);
 
     return (
-        <div className="sidebar-div">Future Sidebar here</div>
-    )
+        <>
+            <button className="toggle-button" onClick={toggleSideBar}>☰</button>
+            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                <ul className="sidebar-links">
+                    <li><Link to="/navbar" onClick={toggleSideBar}>NavBar</Link></li>
+                    <li><Link to="/sidebar" onClick={toggleSideBar}>SideBar</Link></li>
+                    <li><Link to="/search" onClick={toggleSideBar}>Search</Link></li>
+                    <li><Link to="/form" onClick={toggleSideBar}>Form</Link></li>
+                </ul>
+            </div>
+        </>
+    );
 }
 
 export default SideBar;
