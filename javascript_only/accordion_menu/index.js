fb41edd07cca4184513b2b5a5547aa8269c98140
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-08 20:29:25 -0500

    added the necessary javascript to toggle the opening and closing of the accordion

---
+++ javascript_only/accordion_menu/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll(".accordion-button")
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function() {
+            this.classList.toggle("active")
+
+            const content = this.nextElementSibling;
+
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px"
+            }
+        })
+    })
+})
